4925b542e33eae974980dca8e6277b1418e929bb
ldholl <lacy.d.holleman@gmail.com>
2022-08-09 17:17:11 -0400

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,23 +1,33 @@
+window.alert("Welcome to Robot Gladiators!");
+
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerMoney = 10;
-
-//check to see if the value is greater than 0
-if (playerHealth > 0){
-    console.log("Your robot is still alive!");
-}
-
 var playerAttack = 10;
 
-//You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function(){
-    window.alert("Welcome to Robot Gladiators!");
+
+
+//Game states:
+//"Win" - Player robot has defeated all enemy robots
+// *Fight all enemy-robots
+// *Defeat each enemy-robot
+//"LOSE" - player robot's health is zero or less
+
+
+
+for (var i = 0; i < enemyNames.length; i++){
+    console.log(enemyNames[i]);
+    console.log(i);
+    console.log(enemyNames[i] + " is at " + i + " index.");
+}
+
+var fight = function(enemyName){
+// fight function statements
+
 //Ask the player whether they want to fight   
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
@@ -45,19 +55,22 @@ var fight = function(){
         if (playerHealth <= 0) {
           window.alert(playerName + " has died!");
         } else {
-          window.alert(playerName + " still has " + playerHealth + " health left.");
-        }
+          window.alert("Your robot lived! " + playerName + " still has " + playerHealth + " health left.");
+                }
         // if player choses to skip
       } else if (promptFight === "skip" || promptFight === "SKIP") { 
             var confirmSkip = window.confirm("Are you sure you'd like to quit?");
                 if (confirmSkip){
                 window.alert(playerName + " has chosen to skip the fight. Goodbye!");
                 playerMoney = playerMoney - 2;
-      } 
+                                } 
                 else {fight();}}
      else {
         window.alert("You need to choose a valid option. Try again!");
-      }}
+      }
+    }
 
+for (var i = 0; i < enemyNames.length; i++){
 
-fight();
+    fight(enemyNames[i]);
+}
